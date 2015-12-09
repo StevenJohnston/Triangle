3e48cbe8c04ed1599f737ec1b6eb5125b2947141
Steven johnston <wheatthins@hotmail.ca>
2015-12-09 00:29:35 -0500

    Added get/set methods for all sides and angles

--- Triangle/Triangle/Triangle.cs
+++ Triangle/Triangle/Triangle.cs
@@ -10,6 +10,111 @@ namespace TriangleNS
     {
         double sideOne, sideTwo, sideThree;
         double angleOne, angleTwo, angleThree;
+
+        public double SideOne
+        {
+            get
+            {
+                return sideOne;
+            }
+
+            set
+            {
+                sideOne = value;
+            }
+        }
+
+        public double SideTwo
+        {
+            get
+            {
+                return sideTwo;
+            }
+
+            set
+            {
+                sideTwo = value;
+            }
+        }
+
+        public double SideThree
+        {
+            get
+            {
+                return sideThree;
+            }
+
+            set
+            {
+                sideThree = value;
+            }
+        }
+
+        public double AngleOne
+        {
+            get
+            {
+                return AngleOne1;
+            }
+
+            set
+            {
+                AngleOne1 = value;
+            }
+        }
+
+        public double AngleTwo
+        {
+            get
+            {
+                return AngleTwo1;
+            }
+
+            set
+            {
+                AngleTwo1 = value;
+            }
+        }
+
+        public double AngleOne1
+        {
+            get
+            {
+                return angleOne;
+            }
+
+            set
+            {
+                angleOne = value;
+            }
+        }
+
+        public double AngleTwo1
+        {
+            get
+            {
+                return angleTwo;
+            }
+
+            set
+            {
+                angleTwo = value;
+            }
+        }
+
+        public double AngleThree
+        {
+            get
+            {
+                return angleThree;
+            }
+
+            set
+            {
+                angleThree = value;
+            }
+        }
+
         double GetHypotenuse()
         {
             return 0;

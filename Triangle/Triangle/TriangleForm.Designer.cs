101ee42e641a2077ba9efcbd3503f557e816fd27
Steven johnston <wheatthins@hotmail.ca>
2015-12-09 00:12:04 -0500

    Finished designer for Triangle

---
+++ Triangle/Triangle/TriangleForm.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Triangle
+{
+    partial class TriangleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRightAngle = new System.Windows.Forms.Button();
+            this.btnMissingAngle = new System.Windows.Forms.Button();
+            this.txtLengthTwo = new System.Windows.Forms.TextBox();
+            this.txtLengthOne = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtLengthThree = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.btnArea = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRightAngle
+            // 
+            this.btnRightAngle.Location = new System.Drawing.Point(79, 81);
+            this.btnRightAngle.Name = "btnRightAngle";
+            this.btnRightAngle.Size = new System.Drawing.Size(99, 23);
+            this.btnRightAngle.TabIndex = 0;
+            this.btnRightAngle.Text = "Hypotenuse";
+            this.btnRightAngle.UseVisualStyleBackColor = true;
+            // 
+            // btnMissingAngle
+            // 
+            this.btnMissingAngle.Location = new System.Drawing.Point(257, 81);
+            this.btnMissingAngle.Name = "btnMissingAngle";
+            this.btnMissingAngle.Size = new System.Drawing.Size(99, 23);
+            this.btnMissingAngle.TabIndex = 1;
+            this.btnMissingAngle.Text = "Missing Angle";
+            this.btnMissingAngle.UseVisualStyleBackColor = true;
+            // 
+            // txtLengthTwo
+            // 
+            this.txtLengthTwo.Location = new System.Drawing.Point(79, 55);
+            this.txtLengthTwo.Name = "txtLengthTwo";
+            this.txtLengthTwo.Size = new System.Drawing.Size(100, 20);
+            this.txtLengthTwo.TabIndex = 3;
+            // 
+            // txtLengthOne
+            // 
+            this.txtLengthOne.Location = new System.Drawing.Point(79, 29);
+            this.txtLengthOne.Name = "txtLengthOne";
+            this.txtLengthOne.Size = new System.Drawing.Size(100, 20);
+            this.txtLengthOne.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(257, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(257, 55);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // txtLengthThree
+            // 
+            this.txtLengthThree.Enabled = false;
+            this.txtLengthThree.Location = new System.Drawing.Point(79, 139);
+            this.txtLengthThree.Name = "txtLengthThree";
+            this.txtLengthThree.Size = new System.Drawing.Size(100, 20);
+            this.txtLengthThree.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(257, 110);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 8;
+            // 
+            // btnArea
+            // 
+            this.btnArea.Location = new System.Drawing.Point(79, 110);
+            this.btnArea.Name = "btnArea";
+            this.btnArea.Size = new System.Drawing.Size(99, 23);
+            this.btnArea.TabIndex = 9;
+            this.btnArea.Text = "Area";
+            this.btnArea.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Length one";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Length Two";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Angle One";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Angle Two";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(76, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Right Angle";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(254, 10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Not Right Angle";
+            // 
+            // TriangleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 199);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnArea);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.txtLengthThree);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtLengthOne);
+            this.Controls.Add(this.txtLengthTwo);
+            this.Controls.Add(this.btnMissingAngle);
+            this.Controls.Add(this.btnRightAngle);
+            this.Name = "TriangleForm";
+            this.Text = "Triangle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRightAngle;
+        private System.Windows.Forms.Button btnMissingAngle;
+        private System.Windows.Forms.TextBox txtLengthTwo;
+        private System.Windows.Forms.TextBox txtLengthOne;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtLengthThree;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button btnArea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+

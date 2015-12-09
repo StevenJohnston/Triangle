90163bb1c0f8e87913860be283b1f5af2b3cbadc
Steven johnston <wheatthins@hotmail.ca>
2015-12-09 01:21:32 -0500

    Added comments

--- Triangle/Triangle Test/UnitTest1.cs
+++ Triangle/Triangle Test/UnitTest1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+    Name: Steven Johnston
+    Date: 12/9/2015
+    Assignment: #3
+*/
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TriangleNS;
 namespace Triangle_Test
@@ -6,6 +11,9 @@ namespace Triangle_Test
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Tests the set and get for  SideOne.
+        /// </summary>
         [TestMethod]
         public void TestSetGetSideLengthOne()
         {
@@ -13,6 +21,9 @@ namespace Triangle_Test
             triangle.SideOne = 1;
             Assert.AreEqual( 1, triangle.SideOne);
         }
+        /// <summary>
+        /// Tests the set and get for  SideTwo.
+        /// </summary>
         [TestMethod]
         public void TestSetGetSideLengthTwo()
         {
@@ -20,6 +31,9 @@ namespace Triangle_Test
             triangle.SideTwo = 1;
             Assert.AreEqual(1, triangle.SideTwo);
         }
+        /// <summary>
+        /// Tests the set and get for  AngleOne.
+        /// </summary>
         [TestMethod]
         public void TestSetGetAngleOne()
         {
@@ -27,6 +41,9 @@ namespace Triangle_Test
             triangle.AngleOne = 10.5;
             Assert.AreEqual( 10.5,triangle.AngleOne);
         }
+        /// <summary>
+        /// Tests the set and get for  AngleTwo.
+        /// </summary>
         [TestMethod]
         public void TestSetGetAngleTwo()
         {
@@ -34,6 +51,9 @@ namespace Triangle_Test
             triangle.AngleTwo = 10.5;
             Assert.AreEqual(10.5,triangle.AngleTwo);
         }
+        /// <summary>
+        /// Tests the GetHypotenuse.
+        /// </summary>
         [TestMethod]
         public void TestGetHypotenuse()
         {
@@ -42,6 +62,9 @@ namespace Triangle_Test
             triangle.SideTwo = 4;
             Assert.AreEqual(5, triangle.GetHypotenuse());
         }
+        /// <summary>
+        /// Tests the GetArea.
+        /// </summary>
         [TestMethod]
         public void TestGetArea()
         {
@@ -50,7 +73,9 @@ namespace Triangle_Test
             triangle.SideTwo = 4;
             Assert.AreEqual(6, triangle.GetArea());
         }
-
+        /// <summary>
+        /// Tests the GetMissingAngle.
+        /// </summary>
         [TestMethod]
         public void TestGetMissingAngle()
         {

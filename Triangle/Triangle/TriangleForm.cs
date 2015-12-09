13a486d3aca176f596120c131b70c3b6c0f7f193
Steven johnston <wheatthins@hotmail.ca>
2015-12-09 01:21:42 -0500

    Added comments

--- Triangle/Triangle/TriangleForm.cs
+++ Triangle/Triangle/TriangleForm.cs
@@ -23,21 +23,33 @@ namespace TriangleNS
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Calculates the Hypotenuse of the triangle
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void btnRightAngle_Click(object sender, EventArgs e)
         {
             myTriangle.SideOne = Convert.ToDouble(txtLengthOne.Text);
             myTriangle.SideTwo = Convert.ToDouble(txtLengthTwo.Text);
             txtLengthThree.Text = myTriangle.GetHypotenuse().ToString();
         }
-
+        /// <summary>
+        /// Calculates the Area of the triangle
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void btnArea_Click(object sender, EventArgs e)
         {
             myTriangle.SideOne = Convert.ToDouble(txtLengthOne.Text);
             myTriangle.SideTwo = Convert.ToDouble(txtLengthTwo.Text);
             txtLengthThree.Text = myTriangle.GetArea().ToString();
         }
-
+        /// <summary>
+        /// calculates the missing angle of the triangle
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void btnMissingAngle_Click(object sender, EventArgs e)
         {
             myTriangle.AngleOne = Convert.ToDouble(txtAngleOne.Text);

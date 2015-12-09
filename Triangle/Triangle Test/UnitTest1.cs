ce5e9ded96c1d3a1f297687c27d1d2726bec3f4f
Steven johnston <wheatthins@hotmail.ca>
2015-12-09 00:31:48 -0500

    TEST >> TestRightAngleHypotenuse fail due to function being blank

---
+++ Triangle/Triangle Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleNS;
+namespace Triangle_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRightAngleHypotenuse()
+        {
+            Triangle triangle = new Triangle();
+            Assert.AreEqual(triangle.GetHypotenuse(), 1);
+        }
+    }
+}
